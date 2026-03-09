02febf6d3b5c3d452e15153ea602e3e8e97eb147
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-03-09 09:08:53 -0600

    Commit. 1
    Adding the api to the service.

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
+  private http = inject(HttpClient);
 
   jobs: string[] = [
     'Frontend Developer',
@@ -17,4 +21,9 @@ export class DataService {
 
   yearsExperience: number = 3.4567;
 
+  private githubUrl = 'https://api.github.com/users/MiguelCuevasss/repos';
+
+  getRepos(): Observable<any[]> {
+    return this.http.get<any[]>(this.githubUrl);
+  }
 }
