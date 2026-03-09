4f29af689eea85b8126627492a526615993497a3
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-03-09 09:43:46 -0600

    Commit 1.2
    Adding the api to the new service.

--- src/app/services/github.ts
+++ src/app/services/github.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Github {
-  
+export class GithubService {
+  private http = inject(HttpClient);
+
+  private apiUrl = 'https://api.github.com/users/MiguelCuevasss/repos';
+
+  getRepos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
