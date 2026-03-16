98f3cbc2afcdb695742f053ad88f465ea5aafcf7
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-03-16 07:41:23 -0600

    Commit 3
    Creating the path for the github api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { Buttons } from './components/buttons/buttons';
 import { Diplomas } from './components/diplomas/diplomas';
 import { Studies } from './components/studies/studies';
 
+import { Projects } from './components/projects/projects';
+
 export const routes: Routes = [
   { path: 'header', component: Header },
 
@@ -29,5 +31,11 @@ export const routes: Routes = [
     ]
   },
 
-  { path: '', redirectTo: 'header', pathMatch: 'full' }
+  { path: '', redirectTo: 'header', pathMatch: 'full' },
+
+  {
+    path: 'projects',
+    component: Projects
+  }
+
 ];
